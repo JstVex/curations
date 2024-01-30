60de94b4acaf5fe6b5a8262704b0d20243ce1eef
JstVex <heinhtetlulu14@gmail.com>
2024-01-29 22:40:50 -0800

    Display title, subcategories and cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import CardsContainer from "@/components/card/cards-container";
+import Main from "@/components/layout";
+import Subcategories from "@/components/ui/subcategories";
+import TitleSection from "@/components/ui/title-section";
+
 export default function Home() {
   return (
-    <main>
-      curations
-    </main>
+    <Main>
+      <TitleSection title="web developers, designers and programmers" />
+      <Subcategories />
+      <CardsContainer />
+    </Main>
   );
 }
