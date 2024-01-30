5a2d3ec0099147be9fa9566b37791bceb00460db
JstVex <heinhtetlulu14@gmail.com>
2024-01-29 16:41:02 -0800

    Add width fields to handle max width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,11 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      maxWidth: {
+        main: "1100px"
       },
+      width: {
+        main: "1100px"
+      }
     },
   },
   plugins: [],
