7c362dd62ff1a9abe3df451dd97d25c36078ad2c
JstVex <heinhtetlulu14@gmail.com>
2024-02-05 13:02:28 -0800

    Use unique categories instead and filter on select

--- components/ui/subcategories.tsx
+++ components/ui/subcategories.tsx
@@ -1,31 +1,19 @@
-const subcategories = [
-    "All",
-    "Frontend",
-    "Backend",
-    "Portfolio",
-    "Inspiration",
-    "Repositories",
-    "Tools",
-    "Libraries",
-    "Frameworks",
-    "UI Kits",
-    "Typography",
-    "Icons",
-    "Repositories",
-    "Tools",
-    "Libraries",
-    "Frameworks",
-    "UI Kits",
-    "Typography",
-    "Icons"
-]
+interface SubcategoriesProps {
+    subcategories: string[];
+    onSelect: (subcategory: string) => void;
+    selectedSubcategory: string;
+}
 
-const Subcategories = () => {
+const Subcategories: React.FC<SubcategoriesProps> = ({ subcategories, onSelect, selectedSubcategory }) => {
     return (
         <section className="mt-16 mb-4">
             <div className="flex items-center text-zinc-400 overflow-x-auto no-scrollbar">
                 {subcategories.map((subcategory, index) => (
-                    <div key={index} className="w-auto min-w-max cursor-pointer rounded-full transition-colors px-4 py-2 hover:bg-zinc-900 hover:text-white">
+                    <div
+                        key={index}
+                        className={`w-auto min-w-max cursor-pointer rounded-full transition-colors px-4 py-2  hover:text-white ${selectedSubcategory === subcategory ? "bg-zinc-800 text-white" : "hover:bg-zinc-900"}`}
+                        onClick={() => onSelect(subcategory)}
+                    >
                         {subcategory}
                     </div>
                 ))}
