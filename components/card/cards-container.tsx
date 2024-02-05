c078e3ad4f14ac8c7592a60ea14cf86585e887d8
JstVex <heinhtetlulu14@gmail.com>
2024-02-05 13:03:22 -0800

    Add index for unique key

--- components/card/cards-container.tsx
+++ components/card/cards-container.tsx
@@ -8,8 +8,8 @@ const CardsContainer: React.FC<CardsContainerProps> = ({ resources }) => {
     return (
         <>
             <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 px-3">
-                {resources.map((resource: any) => {
-                    return <Card key={resource.title} resource={resource} />;
+                {resources.map((resource: any, index: number) => {
+                    return <Card key={index} resource={resource} />;
                 })}
             </div>
         </>
