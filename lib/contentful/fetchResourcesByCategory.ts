9b0b7a7f98e9f2e390cfaeba6637577dafed5867
JstVex <heinhtetlulu14@gmail.com>
2024-02-06 13:14:41 -0800

    Fetch using entry instead of content delivery api

--- lib/contentful/fetchResourcesByCategory.ts
+++ lib/contentful/fetchResourcesByCategory.ts
@@ -1,14 +1,12 @@
+import { client } from "./client";
+
 export default async function fetchResourcesByCategory(category: string) {
-    const spaceId = process.env.CONTENTFUL_SPACE_ID;
-    const accessToken = process.env.CONTENTFUL_ACCESS_TOKEN;
-    const url = `https://cdn.contentful.com/spaces/${spaceId}/environments/master/entries?access_token=${accessToken}&content_type=resource&fields.category=${category}&include=5`;
     try {
-        const response = await fetch(url);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data.items;
+        const res = await client.getEntries({
+            content_type: "resource",
+            "fields.category": category,
+        });
+        return res.items;
     } catch (error) {
         console.error("Error fetching data from Contentful:", error);
         return [];
