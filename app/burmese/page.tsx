9c72f3a2ae612bad2444ecb0fde27fc8bda127d5
JstVex <heinhtetlulu14@gmail.com>
2024-02-05 17:25:58 -0800

    Create page for burmese resources

---
+++ app/burmese/page.tsx
@@ -0,0 +1,17 @@
+import Main from "@/components/layout";
+import SubcatClient from "@/components/ui/subcat-client";
+import TitleSection from "@/components/ui/title-section";
+import fetchResourcesByCategory from "@/lib/contentful/fetchResourcesByCategory";
+import getUniqueSubcategories from "@/lib/contentful/getUniqueSubcategories";
+
+export default async function Burmese() {
+    const resources = await fetchResourcesByCategory("Burmese");
+    const uniqueSubcats = await getUniqueSubcategories(resources);
+
+    return (
+        <Main>
+            <TitleSection title="burmese developer community" />
+            <SubcatClient initialResources={resources} subcategories={uniqueSubcats} />
+        </Main>
+    );
+}
