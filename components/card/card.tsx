697526f097c7b203ea67f2b81e14c188b81a4932
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-06-20 23:14:44 -0700

    Fix the possible production error on displaying images

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -8,12 +8,14 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ resource }) => {
     const { title, image, slug } = resource.fields;
 
+    const imageUrl = `https:${image.fields.file.url}`;
+
     return (
         <div className="flex flex-col space-y-1">
             <Link href={`/resources/${slug}`}>
                 <Image
                     className="w-full h-full object-cover rounded-lg border border-zinc-800"
-                    src={`https://${image.fields.file.url}`}
+                    src={imageUrl}
                     quality={100}
                     width={200}
                     height={125}
