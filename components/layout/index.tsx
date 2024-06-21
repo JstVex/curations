f9ae0160cd3571316c23725003cc77a168db5dd3
Lu Lu, Hein Htet <heinhtetlulu14@gmail.com>
2024-06-20 22:54:01 -0700

    Update footer layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,14 +6,18 @@ interface MainProps {
 
 const Main: React.FC<MainProps> = ({ children }) => {
     return (
-        <>
-            <main className="flex justify-center px-3 sm:px-4 md:px-6 py-10">
-                <div className="w-full max-w-main grow">
-                    {children}
-                    <Footer />
-                </div>
-            </main>
-        </>
+        <div className="flex flex-col min-h-[90vh]">
+            <div className="flex-grow flex flex-col justify-between">
+                <main className="flex justify-center px-3 sm:px-4 md:px-6 py-10 flex-grow">
+                    <div className="w-full max-w-main">
+                        {children}
+                    </div>
+                </main>
+            </div>
+            <div className="w-full max-w-main mx-auto">
+                <Footer />
+            </div>
+        </div>
     );
 }
 
